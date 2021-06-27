f3356bae122e86ca9767891492841d0a987958c2
Alsev <Alsev@DARKABY-LAPTOP-PRO.home>
2021-06-27 19:07:07 +0200

    Add comments to Piece.java

--- ynov/src/com/ynov/darkaby/pieces/Piece.java
+++ ynov/src/com/ynov/darkaby/pieces/Piece.java
@@ -1,3 +1,7 @@
+/*
+ * Main class for all pieces of the chessboard
+ */
+
 package com.ynov.darkaby.pieces;
 
 import com.ynov.darkaby.CellColor;
@@ -5,12 +9,13 @@ import com.ynov.darkaby.CellColor;
 public abstract class Piece {
 
 	// For this variable to be visible and usable to its subclasses, we use the protected keyword
-	protected CellColor _color;
+	protected CellColor _color; // Color of the piece
 
 	public Piece(CellColor color) {
 		this._color = color;
 	}
 
+	// Draw the piece on the chessboard
 	public abstract void display(int x, int y);
 
 	@Override
